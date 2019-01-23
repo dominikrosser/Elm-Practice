0547aa88a09187310a1cc1aeb4804e3fd72dca61
Dominik Rosser <dominik.rosser@protonmail.ch>
2019-01-23 20:14:45 +0100

    SignupForm updated looks with inline styles

--- SignupForm/src/Signup.elm
+++ SignupForm/src/Signup.elm
@@ -1,8 +1,8 @@
 module Signup exposing (User, init, initialModel, main, update, view)
 
 import Browser
-import Html exposing (Html, button, div, form, h1, input, text)
-import Html.Attributes exposing (id, type_, style)
+import Html exposing (Attribute, Html, button, div, form, h1, input, text)
+import Html.Attributes exposing (id, style, type_)
 import Http
 
 
@@ -37,48 +37,67 @@ init _ =
 view : User -> Html msg
 view user =
     div []
-        [ h1 [ Html.Attributes.style "padding-left" "3cm" ]
-             [ text "Sign up" ] 
-        , Html.form []
+        [ h1 [ headerStyle ]
+            [ text "Sign up" ]
+        , Html.form formStyles
             [ div []
                 [ text "Name"
-                , input
-                    [ id "name"
-                    , type_ "text"
-                    ]
-                    []
+                , input ([ id "name", type_ "text" ] ++ inputTextStyles) []
                 ]
             , div []
                 [ text "Email"
-                , input
-                    [ id "email"
-                    , type_ "email"
-                    ]
-                    []
+                , input ([ id "email", type_ "email" ] ++ inputTextStyles) []
                 ]
             , div []
                 [ text "Password"
-                , input
-                    [ id "password"
-                    , type_ "password"
-                    ]
-                    []
+                , input ([ id "password", type_ "password" ] ++ inputTextStyles) []
                 ]
             , div []
                 [ button
-                    [ type_ "submit" ]
+                    ([ type_ "submit" ] ++ buttonStyles)
                     [ text "Create my account" ]
                 ]
             ]
         ]
 
+
 headerStyle : Attribute msg
 headerStyle =
-  style "padding-left" "3cm"
+    style "padding-left" "3cm"
 
-formStyle : Attribute msg
-formStyle =
-  style "todo" "todo"
+
+formStyles : List (Attribute msg)
+formStyles =
+    [ style "border-radius" "5px"
+    , style "background-color" "#f2f2f2"
+    , style "padding" "20px"
+    , style "width" "300px"
+    ]
+
+
+inputTextStyles : List (Attribute msg)
+inputTextStyles =
+    [ style "display" "block"
+    , style "width" "260px"
+    , style "padding" "12px 20px"
+    , style "margin" "8px 0"
+    , style "border" "none"
+    , style "border-radius" "4px"
+    ]
+
+
+buttonStyles : List (Attribute msg)
+buttonStyles =
+    [ style "width" "300px"
+    , style "background-color" "#397cd5"
+    , style "color" "white"
+    , style "padding" "14px 20px"
+    , style "margin-top" "10px"
+    , style "border" "none"
+    , style "border-radius" "4px"
+    , style "font-size" "16px"
+    ]
+
 
 update : msg -> User -> ( User, Cmd Msg )
 update msg model =
